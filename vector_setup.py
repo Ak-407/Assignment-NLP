9eb6baa1984571df57450dc64120937e0a22f045
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2025-10-07 08:07:57 +0530

    first commit

---
+++ vector_setup.py
@@ -0,0 +1,40 @@
+import psycopg2
+import numpy as np
+from config import DB_CONFIG
+
+# Dummy embedding generator (replace with OpenAI later)
+def get_embedding(text):
+    return np.random.rand(1536).astype(float)  # ensure floats
+
+
+# Connect to DB
+conn = psycopg2.connect(**DB_CONFIG)
+cur = conn.cursor()
+
+# it will get given data in paramerters from database 
+cur.execute("SELECT id, name FROM products")
+for pid, name in cur.fetchall():
+    # it will convert into numpy array which cannot be directly stored in database
+    emb = get_embedding(name)
+    # so will use this to converrt numpy array into a list
+    emb_list = [float(x) for x in emb]  # float list
+    cur.execute(
+        "UPDATE products SET embedding = %s WHERE id = %s",
+        (emb_list, pid)
+    )
+
+
+# similarly for orders
+cur.execute("SELECT id, customer_name FROM orders")
+for oid, cname in cur.fetchall():
+    emb = get_embedding(cname)
+    emb_list = [float(x) for x in emb]
+    cur.execute(
+        "UPDATE orders SET embedding = %s WHERE id = %s",
+        (emb_list, oid)
+    )
+
+conn.commit()
+cur.close()
+conn.close()
+print("Vector embeddings populated successfully!")
